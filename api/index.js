ae6f5ca400ac8ab96bf9f60234f20ee349829372
nicovon24 <nicovon24@gmail.com>
2023-02-28 11:48:04 -0300

    alter true change

--- api/index.js
+++ api/index.js
@@ -25,7 +25,7 @@ const {PORT} = process.env
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(async () => {
-  await saveAllGenres()
+  await saveAllGenres() //true
   await saveAllPlatforms()
   server.listen(PORT, () => {
     console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
